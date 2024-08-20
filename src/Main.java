990eaab8c2835992f8f11e30df70dfcdbd1cd300
FelipeMarcial <felipe.mt@outlook.com>
2024-08-20 00:02:57 -0300

    Pequenas alterações

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		venilton.setNome("Felipe");
 		
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
 
-		cc.depositar(100);
+		cc.depositar(500);
 		cc.transferir(100, poupanca);
 		
 		cc.imprimirExtrato();
